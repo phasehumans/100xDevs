92981bc7e756d52d3aaab88bc497e3101477f726
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-10-27 11:26:18 +0530

    week 4 middlewares

--- week-04/middlewares/02-authmiddleware.js
+++ week-04/middlewares/02-authmiddleware.js
@@ -9,6 +9,15 @@ const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 1
 // Middleware to check for a valid API key
 function authenticateAPIKey(req, res, next) {
     //  authenticate APIKey here
+
+    const key= req.headers('apikey')
+    if(key && key === VALID_API_KEY){
+        return next()
+    }else{
+        return res.status(400).json({
+            msg: "Invalid or missing API key "
+        })
+    }
 }
 
 app.use(authenticateAPIKey);
