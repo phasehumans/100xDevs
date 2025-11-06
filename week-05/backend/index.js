99ab5900c4e14c563953973e246da233dd7e4995
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-11-06 17:02:09 +0530

    added routed and setup basic struct

--- week-05/backend/index.js
+++ week-05/backend/index.js
@@ -1,1 +1,20 @@
 // start writing from here
+const express = require('express')
+const { todoRouter } = require('./routes/todo')
+const { userRouter } = require('./routes/user')
+const { default: mongoose } = require('mongoose')
+
+const app = express()
+app.use(express.json())
+
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/todo', todoRouter)
+
+
+async function dbconnect(){
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000)
+    console.log('listening on port 3000')
+}
+
+dbconnect()
--- week-05/backend/routes/todo.js
+++ week-05/backend/routes/todo.js
@@ -1,1 +1,12 @@
 //  start writing your code from here
+const {Router} = require('express')
+const todoRouter = Router()
+
+todoRouter.post('/', (req, res) => {
+
+})
+
+
+module.exports = {
+    todoRouter : todoRouter
+}
