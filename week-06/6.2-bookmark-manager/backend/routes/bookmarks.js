95b5b789342dce721934fa8b0b4008ae73f469f2
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-11-07 13:45:50 +0530

    week 6 done

--- week-06/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-06/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -1,13 +1,35 @@
 let bookmarks = []; // in memory space
+let count = 0
+export async function addBookmark(req,res){
+   const bookmark = req.body.bookmark
 
-export async function addBookmark(req,res,next){
-// write here
+    if(!bookmark){
+        res.json({
+            message : "bookmark is required"
+        })
+        return
+    }
+
+    bookmark = {
+        bookmark : bookmark,
+        id : count++
+    }
+    bookmarks.push(bookmark)
+
 }
 
-export async function deleteBookmark(req,res,next){
-// write here
+export async function deleteBookmark(req,res){
+    const todoID = req.params.id
+
+    bookmarks.splice(bookmarks.id(todoID))
+
+    res.json({
+        message : "bookmark is deleted"
+    })
 }
 
-export async function getAllBookmarks(req,res,next){
-// write here
+export async function getAllBookmarks(req,res){
+    res.json({
+        bookmarks : bookmarks
+    })
 }
